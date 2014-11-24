963cf7a3f033457ec8b36749f8cf361359ee685e
Sylvain Just <sy.just@i-sloth.com>
2014-11-24 13:11:47 +0100

    modified:   bin/jsonify.sh

--- bin/jsonify.sh
+++ bin/jsonify.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-python -m json.tool $1
+function noFile() {
+	local f="$@"
+	echo "I need a file as argument"
+	if [ ! -z "$f" ] ; then
+		echo "'$f' is not a valid file"
+	fi
+	exit 1
+}
+function jsonify() {
+	python -m json.tool "$@"
+}
+file="$@"
+if [ ! -z "$file" ] ; then
+	if [ -e "$file" ] ; then
+		jsonify "$file"
+	else
+		noFile $file
+	fi
+else
+	noFile
+fi
