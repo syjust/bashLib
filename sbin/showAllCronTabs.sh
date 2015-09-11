3f239979ee29bf9f53c7c5d369c7b035a2d054d9
Sylvain Just / Eone <eone+sylvain@i-sloth.com>
2015-09-11 17:57:03 +0200

    showAllCrontabs with (or without) /etc/cron* cron scripts

--- sbin/showAllCronTabs.sh
+++ sbin/showAllCronTabs.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+uid=`id -u`
+if [ $uid -ne 0 ] ; then
+	echo "root only script, use sudo or go your way !"
+	exit 1
+fi
+
+quiet=0
+user_only=0
+while [ ! -z "$1" ] ; do
+	case $1 in
+		-q|--quiet) quiet=1 ; shift ;;
+		-u|--user-only) user_only=1 ; shift ;;
+		*) echo "'$1' : arg not recognized (try --quiet or --user-only)" ; exit 1 ;;
+	esac
+done
 
 for user in $(cut -f1 -d: /etc/passwd); do
 	ctab=`crontab -u $user -l 2>&1 | grep -vE "^[[:blank:]]*#"`
@@ -9,13 +24,45 @@ for user in $(cut -f1 -d: /etc/passwd); do
 		echo "$ctab"
 		echo
 	else
-		if [ "x$1" != "x-q" ] ; then
+		if [ $quiet -eq 0 ] ; then
 			echo "-- $ctab"
+			echo
 		fi
 	fi
 done
 
-if [ "x$1" != "x-q" ] ; then
-	echo
-	echo "add '-q' as argument for show only efficient crontab users"
+if [ $user_only -eq 0 ] ; then
+
+	for cron_tab in /etc/cron.d/* /etc/crontab ; do
+		ctab=`grep -vE "^[[:blank:]]*#" $cron_tab`
+		echo "========================================"
+		echo "crontab for $cron_tab :"
+		echo "----------------------------------------"
+		echo "$ctab"
+		echo
+	done
+
+	for cron_tab in /etc/cron.{hourly,daily,monthly} ; do
+		scripts="`ls $cron_tab`"
+		if [ ! -z "$scripts" ] ; then
+			echo "========================================"
+			echo "scripts for crontab $cron_tab :"
+			echo "----------------------------------------"
+			echo "$scripts"
+			echo
+		else
+			if [ $quiet -eq 0 ] ; then
+				echo "-- no scripts for crontab $cron_tab"
+				echo
+			fi
+		fi
+	done
+
+fi
+
+if [ $quiet -eq 0 ] ; then
+	echo "add '-q' as argument for show only efficient crontabs"
+fi
+if [ $user_only -eq 0 ] ; then
+	echo "add '-u' as argument for show only users crontabs"
 fi
