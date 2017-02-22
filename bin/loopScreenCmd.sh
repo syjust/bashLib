4d6f7dedf494bb2be10d0a9eb2b4b96a22011ada
Sylvain Just <sylvain.just.external@watchever.com>
2017-02-22 16:59:33 +0100

    loopScreenCmd screen context as -S arg

--- bin/loopScreenCmd.sh
+++ bin/loopScreenCmd.sh
@@ -5,9 +5,21 @@ if [ ! -z $BASH_LIB -a -d $BASH_LIB ] ; then
   . $BASH_LIB/lib/readLine.sh
 fi
 
+SCREEN_OPT=""
 function execScreenCmd () {
-	screen -X $@
+	echo -n "screen $SCREEN_OPT -X $@ ... "
+ screen $SCREEN_OPT -X $@ \
+  && echo OK \
+  || echo ERROR
 }
+
+# get session arg
+while [ ${#@} -gt 0 ] ; do
+  case $1 in
+    -S|--session) [ ! -z "$2" ] && SCREEN_OPT="-S $2" || quit "-S need session name as argument" ; shift 2 ;;
+    *) break ;;
+  esac
+done
 
 while [ ${#@} -gt 0 ] ; do
 	echo "file : '$1'"
