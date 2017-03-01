80a125ad300bbf02f83082cb1a01dce3f83cf6ce
Sylvain Just <sylvain.just.external@watchever.com>
2017-03-01 14:51:13 +0100

    screen mgmt new scripts

---
+++ lib/helper.inc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+[ -z "$BASH_LIB" ] && echo "bashLib path not found" && exit 1
+source $BASH_LIB/lib/color-minproc.sh
+
+#############
+# FUNCTIONS #
+#############
+quit(){
+  bredn -e "$@"
+  exit 1
+}
+
+#############
+# CONSTANTS #
+#############
+KERNEL="`uname -s`"
+
+###########
+# EXPORTS #
+###########
+export -f quit
+export -p KERNEL
