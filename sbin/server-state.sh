14a3643194b7d147d746d3f37daeb706dc0d3170
Sylvain JUST <sylvain.just@i-sloth.com>
2018-01-04 12:03:54 +0100

    server-state.sh add command in title for each action
    branch:master

--- sbin/server-state.sh
+++ sbin/server-state.sh
@@ -32,25 +32,39 @@ starting() {
 }
 export -f starting
 
+title() {
+    local t="$1"
+    shift
+    log "$U_SEP"
+    log "${t//_/ } : '$@'"
+    log "$D_SEP"
+}
+export -f title
+
 doIt() {
     local t="$1"
     local date="`getDate`:"
     case $t in
         Espace_disques)
+            title "$t" "df -h"
             df -h | sed "s/^/$date    /"
         ;;
         RAM_/_CPU_Usage)
+            title "$t" "top -b -n1"
             top -b -n1 | head -n 20 | sed "s/^/$date    /"
         ;;
         IO_tests)
+            title "$t" "iostat -d 2 5"
             iostat -d 2 5 | sed "s/^/$date    /"
         ;;
         MAJ_des_Paquet)
+            title "$t" "apt-get upgrade --dry-run"
             apt-get update --quiet --assume-yes > /dev/null 2>&1
             apt-get upgrade --dry-run | grep -vE "(Inst|Conf)" | sed "s/^/$date    /"
         ;;
         *)
-            log "    WTF $t"
+            title $t WTF
+            log "    What The F*** $t"
     esac
 }
 export -f doIt
@@ -58,9 +72,6 @@ export -f doIt
 looping() {
     local t
     for t in $TESTS ; do
-        log "$U_SEP"
-        log "${t//_/ } :"
-        log "$D_SEP"
         doIt $t
         log
     done
