b606e680e573b12cd13bb9737a5800ee48db9511
Sylvain Just <sylvain@justdevops.com>
2025-11-14 15:18:45 +0100

    feat: add logo-in-logo size option

--- bin/logo-in-logo.sh
+++ bin/logo-in-logo.sh
@@ -13,8 +13,9 @@ usage(){
     echo "USAGE: $0 [OPTIONS] MAIN_IMAGE IN_IMAGE"
     echo
     echo "Where OPTIONS could be:"
-    echo "  -h|--help     Prints this message and exits without errors."
-    echo "  -d|--debug    Dry Run and print debugs instead of doing the ImageMagick Action."
+    echo "  -h|--help           Prints this message and exits without errors."
+    echo "  -d|--debug          Dry Run and print debugs instead of doing the ImageMagick Action."
+    echo "  -s|--size {SIZE}    Override the default main size (128 that will produce a 128x128px main logo with a 32x32 in-logo image)."
     echo
 }
 export -f usage
@@ -43,7 +44,7 @@ set_args(){
         export IN_IMAGE="$arg"
         return 0
     fi
-    quit "'$arg': WTF!!! Both required args 9'MAIN_IMAGE' and 'IN_IMAGE' are already set!"
+    quit "'$arg': WTF!!! Both required args 'MAIN_IMAGE' and 'IN_IMAGE' are already set!"
 
 }
 export -f set_args
@@ -54,6 +55,7 @@ export -f set_args
 check_variables(){
     local MAIN_IMAGE="$1"
     local IN_IMAGE="$2"
+    local SIZE="$3"
     i=0
     i_ext="th"
     for var in MAIN IN ; do
@@ -72,6 +74,9 @@ check_variables(){
             quit "'${!var_name}': $var_name file (${i}${i_ext} arg) is not a valid file or not readable!"
         fi
     done
+    if [[ ! $SIZE =~ ^[0-9]+$ ]] ; then
+        quit "'$SIZE': invalid {SIZE} (only digits are allowed here)"
+    fi
 }
 export -f check_variables
 # }}}
@@ -79,10 +84,13 @@ export -f check_variables
 # {{{ function run
 # #
 run(){
-    check_variables "$1" "$2"
+    check_variables "$1" "$2" "$3"
 
     local main_image="$1"
     local in_image="$2"
+    local size="$3"
+    local in_size="$(($size/4))"
+    local pad_size="$(($size-$in_size))"
 
     local main_title="$(basename "${main_image%%.*}")"
     local in_title="$(basename "${in_image%%.*}")"
@@ -92,9 +100,9 @@ run(){
     echo -e "\nWorking on $target_title\n"
     if [[ $DEBUG -eq 1 ]] ; then
         echo -e "DEBUG\n"
-        echo "convert -resize 128x128 \"$main_image\" \"$target_main_file\""
-        echo "convert -transparent \#ffffff -resize 32x32 \"$in_image\" \"$target_in_file\""
-        echo "magick \( -page +0+0 \"$target_main_file\" \) \( -page +80+0 \"$target_in_file\" \) -mosaic \"$target_file\""   # top-right
+        echo "convert -resize ${size}x${size} \"$main_image\" \"$target_main_file\""
+        echo "convert -transparent \#ffffff -resize ${in_size}x${in_size} \"$in_image\" \"$target_in_file\""
+        echo "magick \( -page +0+0 \"$target_main_file\" \) \( -page +$pad_size+0 \"$target_in_file\" \) -mosaic \"$target_file\""   # top-right
         #echo "magick \( -page +0+0 \"$target_main_file\" \) \( -page +80+96 \"$target_in_file\" \) -mosaic \"$target_file\"" # bottom-right
         echo "rm -v \"$target_main_file\" \"$target_in_file\""
         rm -v "$target_main_file" "$target_in_file"
@@ -102,10 +110,10 @@ run(){
         return 0
     fi
 
-    convert -resize 128x128 "$main_image" "$target_main_file"
-    convert -transparent \#ffffff -resize 32x32 "$in_image" "$target_in_file"
+    convert -resize ${size}x${size}  "$main_image" "$target_main_file"
+    convert -transparent \#ffffff -resize ${in_size}x${in_size}  "$in_image" "$target_in_file"
     magick \( -page +0+0 "$target_main_file" \) \
-        \( -page +80+0 "$target_in_file" \) \
+        \( -page +$pad_size+0 "$target_in_file" \) \
         -mosaic "$target_file"
     local ret=$?
     rm "$target_main_file" "$target_in_file"
@@ -120,14 +128,16 @@ export -f run
 
 export MAIN_IMAGE=""
 export IN_IMAGE=""
+export SIZE="128"
 DEBUG=0
 while [[ ! -z "$1" ]] ; do
     case $1 in
         -h|--help) usage ; exit 0 ;;
         -d|--debug) DEBUG=1 ;;
+        -s|--size) [[ -z "$2" ]] && quit "--size option needs a valid size argument" || SIZE=$2 ; shift ;;
         *) set_args "$1" ;;
     esac
     shift
 done
 
-run "$MAIN_IMAGE" "$IN_IMAGE"
+run "$MAIN_IMAGE" "$IN_IMAGE" "$SIZE"
